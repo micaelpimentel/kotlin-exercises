a40d76623d5fed8dd8f720005d18593ec65a8cba
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-16 11:47:15 -0400

    Overrided methods in Endereco

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,5 +1,25 @@
-import br.com.alura.bytebank.teste.testaAutenticacaoTiposDiferentes
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    testaAutenticacaoTiposDiferentes()
+    val endereco = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "Alura",
+        cep = "00000-0700"
+    ).also(::println)
+    val enderecoNovo = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "Alura",
+        cep = "00000-0700"
+    ).also(::println)
+
+    println(endereco.equals(enderecoNovo))
+
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
+
+    println(
+        "${endereco.javaClass}@${
+            Integer.toHexString(endereco.hashCode())
+        }"
+    )
 }
