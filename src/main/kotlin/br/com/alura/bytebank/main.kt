ddca4881408632e6a7015181aba84c6c74720fd0
Micael de Sousa Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-19 12:37:33 -0400

    Extracted testaPilha to file

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,35 +1,5 @@
 import br.com.alura.bytebank.exceptions.SaldoInsuficienteException
 
 fun main() {
-    testaComportamentoConta()
+
 }
-
-private fun testaPilhaExecucao() {
-    println("início main")
-    funcao1()
-    println("fim main")
-}
-
-fun funcao1() {
-    println("início funcao1")
-    try {
-        funcao2()
-    } catch (e: SaldoInsuficienteException) {
-        println("Pegou ClassCastException")
-        println(e.message)
-        e.printStackTrace()
-        println(e.cause)
-    }
-    println("fim funcao1")
-}
-
-fun funcao2() {
-    println("início funcao2")
-
-    for (i in 1..5) {
-        println(i)
-        throw SaldoInsuficienteException()
-    }
-    println("fim funcao2")
-}
-
