dbae7d197f6b4cc2d9bb8145f36c07edbed86f15
Micael de Sousa Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-19 11:06:12 -0400

    Test with exceptions

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,7 +1,30 @@
+import br.com.alura.bytebank.modelo.Endereco
 import br.com.alura.bytebank.teste.testaAny
+import java.lang.ClassCastException
 
 fun main() {
-    testaAny()
-    testaCopiaEReferencias()
-    
+    println("início main")
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1() {
+    println("início funcao1")
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        println("Pegou ClassCastException")
+    }
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+
+    for (i in 1..5) {
+        println(i)
+        val endereco = Any()
+        endereco as Endereco
+    }
+    println("fim funcao2")
 }
