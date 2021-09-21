8d86a45d7ac524323ed89934daaf859e17125914
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-21 15:44:31 -0400

    Test with distinct

---
+++ collections/src/main/kotlin/br/com/micael/collections/listas/testaCollections.kt
@@ -0,0 +1,41 @@
+package br.com.micael.collections.listas
+
+fun testaCopia() {
+    val bancoDeNomes = BancoDeNomes()
+    val nomesSalvos: Collection<String> = bancoDeNomes.nomes
+    bancoDeNomes.salva("Alex")
+
+    println(bancoDeNomes.nomes)
+    println(nomesSalvos)
+}
+
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = dados.toList()
+
+    fun salva(nome: String) {
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: MutableCollection<String> = mutableListOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria",
+        "Ana"
+    )
+    nomes.add("Paulo")
+    nomes.remove("Alex")
+    nomes.forEach {
+        println(it)
+    }
+    println(nomes)
+    println(nomes.contains("Alex"))
+    println("Tamanho da coleção: ${nomes.size}")
+}
--- collections/src/main/kotlin/br/com/micael/collections/main.kt
+++ collections/src/main/kotlin/br/com/micael/collections/main.kt
@@ -1,41 +1,14 @@
 package br.com.micael.collections
 
+import br.com.micael.collections.listas.testaCopia
+
 fun main() {
-    val bancoDeNomes = BancoDeNomes()
-    val nomesSalvos: Collection<String> = bancoDeNomes.nomes
-    bancoDeNomes.salva("Alex")
+    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
+    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
 
-    println(bancoDeNomes.nomes)
-    println(nomesSalvos)
-}
+//    assistiramAmbos.addAll(assistiramCursoAndroid)
+//    assistiramAmbos.addAll(assistiramCursoKotlin)
 
-class BancoDeNomes {
-
-    val nomes: Collection<String> get() = dados.toList()
-
-    fun salva(nome: String) {
-        dados.add(nome)
-    }
-
-    companion object {
-        private val dados = mutableListOf<String>()
-    }
-}
-
-fun testaColecao() {
-    val nomes: MutableCollection<String> = mutableListOf(
-        "Alex",
-        "Fran",
-        "Gui",
-        "Maria",
-        "Ana"
-    )
-    nomes.add("Paulo")
-    nomes.remove("Alex")
-    nomes.forEach {
-        println(it)
-    }
-    println(nomes)
-    println(nomes.contains("Alex"))
-    println("Tamanho da coleção: ${nomes.size}")
+    println(assistiramAmbos.distinct())
 }
