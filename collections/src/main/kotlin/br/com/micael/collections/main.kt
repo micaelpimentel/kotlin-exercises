8da4f2551b0515154eb6fa64dc9275b378de781b
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-21 18:13:36 -0400

    Test with grouping

--- collections/src/main/kotlin/br/com/micael/collections/main.kt
+++ collections/src/main/kotlin/br/com/micael/collections/main.kt
@@ -19,6 +19,23 @@ fun main() {
     }
     println(pedidosComFreteGratis)
     println(pedidosComFreteGratis[Pedido(numero = 1, valor = 60.0)])
+
+//    val mapa = pedidos.associateBy { pedido ->
+//        pedido.valor > 50.0
+//    }
+//    println(mapa)
+
+    val pedidosFreteGratisAgrupados = pedidos.groupBy { pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratisAgrupados)
+    println(pedidosFreteGratisAgrupados[true])
+
+    val nomes = listOf("Alex", "Fran", "Ana", "Gui", "Paulo", "Maria", "Mario", "Gisele")
+    val agenda = nomes.groupBy { nome ->
+        nome.first()
+    }
+    println(agenda)
 }
 
 data class Pedido(val numero: Int, val valor: Double)
