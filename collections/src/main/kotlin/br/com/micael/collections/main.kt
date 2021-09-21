d9f846d8cdc551f3274c64447bb66bb62b4c50ac
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-21 14:49:44 -0400

    Implementing bancoDeNomes

--- collections/src/main/kotlin/br/com/micael/collections/main.kt
+++ collections/src/main/kotlin/br/com/micael/collections/main.kt
@@ -1,7 +1,39 @@
 package br.com.micael.collections
 
-import br.com.micael.collections.listas.trabalhandoComListas
-
 fun main() {
-    trabalhandoComListas()
+    val bancoDeNomes = BancoDeNomes()
+    //bancoDeNomes.nomes.add("Alex")
+    bancoDeNomes.salvo("Alex")
+    println(bancoDeNomes.nomes)
+}
+
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = Companion.dados
+
+    fun salvo(nome: String) {
+        Companion.dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: MutableCollection<String> = mutableListOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria",
+        "Ana"
+    )
+    nomes.add("Paulo")
+    nomes.remove("Alex")
+    nomes.forEach {
+        println(it)
+    }
+    println(nomes)
+    println(nomes.contains("Alex"))
+    println("Tamanho da coleção: ${nomes.size}")
 }
