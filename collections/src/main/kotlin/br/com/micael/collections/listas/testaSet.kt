ebb81aa3b419c3c208cb317b56f141a5af5cf55b
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-21 16:51:18 -0400

    Revert "Revert "Test Map-specific functions""
    This reverts commit cefbd22f93c5be16af20164f19249ff0b2075d6b.

---
+++ collections/src/main/kotlin/br/com/micael/collections/listas/testaSet.kt
@@ -0,0 +1,28 @@
+package br.com.micael.collections.listas
+
+fun testaSet() {
+    val assistiramCursoAndroid: MutableSet<String> = mutableSetOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: MutableSet<String> = mutableSetOf("Alex", "Paulo", "Maria")
+//    val assistiramAmbos: Set<String> = assistiramCursoKotlin + assistiramCursoAndroid
+    val assistiramAmbos = mutableSetOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Gui")
+    assistiramAmbos.add("Ana")
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
+
+    println(assistiramCursoAndroid + assistiramCursoKotlin)
+    println(assistiramCursoKotlin union assistiramCursoAndroid)
+
+    println(assistiramCursoAndroid - assistiramCursoKotlin)
+    println(assistiramCursoKotlin - assistiramCursoAndroid)
+    println(assistiramCursoKotlin subtract assistiramCursoAndroid)
+
+    println(assistiramCursoKotlin intersect assistiramCursoAndroid)
+
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet())
+}
