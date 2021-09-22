e1f9951e774b9723ddc29f50b27ba56068313ec1
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-22 16:46:49 -0400

    Test with lambda and anonynmous functions with multiple return values

--- bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -8,6 +8,20 @@ fun main() {
     }
     println(minhaFuncaoLambda(15, 10))
 
+    val calculaBonificacao: (Double) -> Double = lambda@ { salario ->
+        if (salario > 1000.0)
+            return@lambda salario + 50.0
+        return@lambda salario + 100.0
+    }
+    println(calculaBonificacao(1100.0))
+
+    val calculaBonificacaoAnonima = fun(salario: Double): Double {
+        if (salario > 1000.0)
+            return salario + 50.0
+        return salario + 100.0
+    }
+    println(calculaBonificacaoAnonima(950.0))
+
     val minhaFuncaoAnonima: (Int, Int) -> Int = fun(a, b): Int{
         return a + b
     }
