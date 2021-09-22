8781604f87df09d37993be363a166f53ff9f1ed8
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-22 16:11:22 -0400

    Test with anonymous and lambda functions

--- bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,10 +1,24 @@
 
 fun main() {
-    val minhaFuncao: () -> Unit = ::teste
-    println(minhaFuncao())
+    val minhaFuncaoLambda = {
+        println("Executa com o lambda")
+    }
+    println(minhaFuncaoLambda())
 
+    val minhaFuncaoAnonima: () -> Unit = fun(){
+        println("Executa como anonima")
+    }
+    println(minhaFuncaoAnonima())
+}
+
+fun testaTipoFuncaoClasse() {
     val minhaFuncaoClasse = Teste()
     println(minhaFuncaoClasse())
+}
+
+fun testaTipoFuncaoReferencia() {
+    val minhaFuncao: () -> Unit = ::teste
+    println(minhaFuncao())
 }
 
 fun teste() {
