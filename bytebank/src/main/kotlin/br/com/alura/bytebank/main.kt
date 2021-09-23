06f56fac133d3ca2284d6c73c66b4f91933fe322
Micael Pimentel <micael.pimentel@icomp.ufam.edu.br>
2021-09-23 17:17:43 -0400

    Intro to high-order functions

--- bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,45 +1,19 @@
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-//    testaTipoFuncaoClasse()
-//    testaTipoFuncaoReferencia()
+//    val endereco = Endereco(logradouro = "rua vergueiro", numero = 4123)
+//
+//    val enderecoMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+//    println(enderecoMaiusculo)
 
-    val minhaFuncaoLambda = { a: Int, b: Int ->
-        a + b
-    }
-    println(minhaFuncaoLambda(15, 10))
+    val enderecoMaiusculo = Endereco(logradouro = "rua vergueiro", numero = 4123).let { endereco ->
+        "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+    }.also(::println)
 
-    val calculaBonificacao: (Double) -> Double = lambda@ { salario ->
-        if (salario > 1000.0)
-            return@lambda salario + 50.0
-        return@lambda salario + 100.0
-    }
-    println(calculaBonificacao(1100.0))
-
-    val calculaBonificacaoAnonima = fun(salario: Double): Double {
-        if (salario > 1000.0)
-            return salario + 50.0
-        return salario + 100.0
-    }
-    println(calculaBonificacaoAnonima(950.0))
-
-    val minhaFuncaoAnonima: (Int, Int) -> Int = fun(a, b): Int{
-        return a + b
-    }
-    println(minhaFuncaoAnonima(13, 8))
-}
-
-fun testaTipoFuncaoClasse() {
-    val minhaFuncaoClasse = Soma()
-    println(minhaFuncaoClasse(2, 7))
-}
-
-fun testaTipoFuncaoReferencia() {
-    val minhaFuncao = ::soma
-    println(minhaFuncao(3, 4))
-}
-
-fun soma(a: Int, b: Int) = a + b
-
-class Soma : (Int, Int) -> Int {
-    override fun invoke(a: Int, b: Int): Int = a + b
+    listOf(
+        Endereco(complemento = "casa"),
+        Endereco(),
+        Endereco(complemento = "apartamento")
+    ).filter { endereco -> !endereco.complemento.isNullOrEmpty() }
+        .also(::println)
 }
---
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/teste/testaTipoCuncao.kt
@@ -0,0 +1,45 @@
+package br.com.alura.bytebank.teste
+
+fun testaFuncaoAnonima() {
+    val calculaBonificacaoAnonima = fun(salario: Double): Double {
+        if (salario > 1000.0)
+            return salario + 50.0
+        return salario + 100.0
+    }
+    println(calculaBonificacaoAnonima(950.0))
+
+    val minhaFuncaoAnonima: (Int, Int) -> Int = fun(a, b): Int {
+        return a + b
+    }
+    println(minhaFuncaoAnonima(13, 8))
+}
+
+fun testaFuncaoLambda() {
+    val minhaFuncaoLambda = { a: Int, b: Int ->
+        a + b
+    }
+    println(minhaFuncaoLambda(15, 10))
+
+    val calculaBonificacao: (Double) -> Double = lambda@{ salario ->
+        if (salario > 1000.0)
+            return@lambda salario + 50.0
+        return@lambda salario + 100.0
+    }
+    println(calculaBonificacao(1100.0))
+}
+
+fun testaTipoFuncaoClasse() {
+    val minhaFuncaoClasse = Soma()
+    println(minhaFuncaoClasse(2, 7))
+}
+
+fun testaTipoFuncaoReferencia() {
+    val minhaFuncao = ::soma
+    println(minhaFuncao(3, 4))
+}
+
+fun soma(a: Int, b: Int) = a + b
+
+class Soma : (Int, Int) -> Int {
+    override fun invoke(a: Int, b: Int): Int = a + b
+}
